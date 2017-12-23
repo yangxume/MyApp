869652c78e163f6dc8c7ccd65f568d734a6be484
xuyang <xuyang@okay.cn>
2017-12-23 15:41:26 +0800

    fragment

---
+++ app/src/main/java/com/okay/myapp/Activity04_Fragment.java
@@ -0,0 +1,28 @@
+package com.okay.myapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Copyright
+ * <p>
+ * Created by xuyang on 17/12/23 15:36
+ * <p>
+ * email xuyangme@126.com
+ * <p>
+ * ${FILENAME}
+ * <p>
+ * Description
+ * <p>
+ * Update records:
+ */
+
+public class Activity04_Fragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity04_fragment);
+    }
+}
---
+++ app/src/main/java/com/okay/myapp/ui/fragment/HeadlinesFragment.java
@@ -0,0 +1,39 @@
+package com.okay.myapp.ui.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.okay.myapp.R;
+
+/**
+ * Copyright
+ * <p>
+ * Created by xuyang on 17/12/23 15:34
+ * <p>
+ * email xuyangme@126.com
+ * <p>
+ * ${FILENAME}
+ * <p>
+ * Description
+ * <p>
+ * Update records:
+ */
+
+public class HeadlinesFragment
+        extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment04_headlines,container,false);
+    }
+}
