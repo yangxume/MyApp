aa5be231a80ab588653641a1dceb1cec2b248bdb
xuyang <xuyang@okay.cn>
2018-10-16 10:31:19 +0800

    【add】kotlin study 001

---
+++ app/src/main/java/com/okay/myapp/Activity12_ConstraintLayout.java
@@ -0,0 +1,28 @@
+package com.okay.myapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Copyright
+ * <p>
+ * Created by xuyang on 18/10/12 14:48
+ * <p>
+ * email xuyangme@126.com
+ * <p>
+ * ${FILENAME}
+ * <p>
+ * Description
+ * <p>
+ * Update records:
+ */
+public class Activity12_ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity12_constraintlayout);
+
+    }
+}
