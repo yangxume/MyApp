235ecc5203d117099c9e9b00513177996047cd36
xuyang <xuyangme@126.com>
2019-03-01 14:39:35 +0800

    update
     classpath 'com.android.tools.build:gradle:3.2.1'
    distributionUrl=https\://services.gradle.org/distributions/gradle-4.6-all.zip

---
+++ android_runtimepermissions/src/main/java/com/xy/android_android_runtimepermissions/MainActivity.java
@@ -0,0 +1,113 @@
+package com.xy.android_android_runtimepermissions;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity
+             implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
+
+    private LinearLayout main_layout;
+    private Button button_open_camera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+        initListener();
+    }
+
+    private void initListener() {
+
+        button_open_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showCameraPreview();
+
+            }
+        });
+    }
+
+    private void showCameraPreview() {
+
+
+        int checkSelfPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+        int permissionGranted = PackageManager.PERMISSION_GRANTED;
+
+        if (checkSelfPermission == permissionGranted){
+
+            Snackbar.make(main_layout,
+                    R.string.camera_permission_available,
+                    Snackbar.LENGTH_SHORT).show();
+
+        }else {
+            requestCameraPermission();
+        }
+
+    }
+
+    private void requestCameraPermission() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with cda button to request the missing permission.
+            Snackbar.make(main_layout, R.string.camera_access_required,
+                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok, new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            PERMISSION_REQUEST_CAMERA);
+                }
+            }).show();
+
+        } else {
+            Snackbar.make(main_layout, R.string.camera_unavailable, Snackbar.LENGTH_SHORT).show();
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }
+    }
+
+    private void initView() {
+
+        main_layout = findViewById(R.id.main_layout);
+        button_open_camera = findViewById(R.id.button_open_camera);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted. Start camera preview Activity.
+                Snackbar.make(mLayout, R.string.camera_permission_granted,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+                startCamera();
+            } else {
+                // Permission request was denied.
+                Snackbar.make(mLayout, R.string.camera_permission_denied,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }    }
+}
