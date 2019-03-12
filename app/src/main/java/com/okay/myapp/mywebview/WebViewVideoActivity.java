0858ca344cf221ffad12df3bf7bd703d6fb7cef6
xuyang <xuyangme@126.com>
2019-03-12 11:06:35 +0800

    del not use code

--- app/src/main/java/com/okay/myapp/mywebview/WebViewVideoActivity.java
+++ app/src/main/java/com/okay/myapp/mywebview/WebViewVideoActivity.java
@@ -7,7 +7,6 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewParent;
 import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
